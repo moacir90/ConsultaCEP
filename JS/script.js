e7843dbea31cfd8f99e2f4c70443d2c4098b8c0f
moacir90 <123340944+moacir90@users.noreply.github.com>
2023-05-18 15:59:09 -0300

    moacir
    base para consulta

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+document.getElementById("btnPesquisar").addEventListener("click", function(){
+    var cep = document.getElementById("cep").value //pega o valor do id cep
+    var url = "https://viacep.com.br/ws/"+ cep + "/json"; //link da api
+    var resultado = document.getElementById("resultado"); //pega a div resultado
+    
+    //função fetch e responsavel por fazer uma busca
+    fetch(url).then(response => response.json()).then(dados => {
+        if(dados.erro){
+            resultado.innerHTML = "CEP não encontrado"
+        }   else{
+            resultado.innerHTML = "Rua: " + dados.logradouro + "Bairro: " + dados.bairro
+        }
+
+    }).catch(error => console.error(error))
+
+})
